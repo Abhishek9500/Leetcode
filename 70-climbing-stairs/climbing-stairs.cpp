933730a9e9eddbc149d249e820a0cddb59fccba8
Abhishek Kumar <abhishek6200singh@gmail.com>
2026-04-09 15:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.88%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findWays(int n,vector<int>&dp){
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n]= findWays(n-1,dp)+findWays(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return findWays(n,dp);
+    }
+};
